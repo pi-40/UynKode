1c4f752d8a6e05d8d7b283acfb42fe795d897a3a
Pi40 <samanthamurray20091984@gmail.com>
2026-07-09 12:33:32 -0400

    Create Form1.Designer.cs

---
+++ src/Form1.Designer.cs
@@ -0,0 +1,35 @@
+namespace UynkodeInterpreter
+{
+    partial class AppForm
+    {
+        private System.ComponentModel.IContainer components = null;
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null)) components.Dispose();
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(AppForm));
+            SuspendLayout();
+            // 
+            // AppForm
+            // 
+            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(0, 0);
+            FormBorderStyle = FormBorderStyle.None;
+            Icon = (Icon)resources.GetObject("$this.Icon");
+            Margin = new Padding(3, 4, 3, 4);
+            Name = "AppForm";
+            ShowInTaskbar = false;
+            WindowState = FormWindowState.Minimized;
+            Load += AppForm_Load;
+            ResumeLayout(false);
+        }
+        #endregion
+    }
+}
